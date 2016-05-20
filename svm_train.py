7ee53f5e51784572791b3ea2d12fc9cfe689fac9
angelakong <akong2@stanford.edu>
2016-05-20 16:47:48 -0700

    Preprocessed all the data so we can call training models

--- svm_train.py
+++ svm_train.py
@@ -17,57 +17,92 @@ from sklearn.svm import SVR
 from scipy.stats import mode
 import matplotlib.pyplot as plt
 
-
 df = pandas.read_csv('yelp_academic_dataset_business.csv', skip_blank_lines=True)
+# There are 77445 training examples, 98 possible features
+
+features = df.columns.values
+
+#for index, feature in enumerate(features):
+#    if "categories" in feature:
+#        print index
+        
+category_index = 20
 
 # Classification vector
 y = df['stars']
 y[y < 4.0] = 0
 y[y >= 4.0] = 1
 
-alc = df["attributes.Alcohol"].replace(['none', 'beer_and_wine', 'full_bar'], [0,1,2])
-alc.fillna(mode(alc).mode[0], inplace = True)
+# Remove the non-restaurants from the data
+for i, category in enumerate(df["categories"]):
+    if "u'Restaurants" not in category:
+        df.drop(df.index[i])
 
-takeout = df["attributes.Take-out"]
-takeout.fillna(0, inplace = True)
-takeout = takeout.astype(int)
+print(df.shape)
 
-waiter = df["attributes.Waiter Service"]
-# If restaurant does not list a benefit, we assume that it lacks it
-waiter.fillna(0, inplace = True)
+# Preprocess the data for modeling
+# Prepare for messy code
+for f in features:
+    # Special code for categories
+    # Drop hours for now (easier to process)
+    delete = ["hours", "business_id", "stars", "type", "categories", "name", "full_address", "BYOB", "neighborhoods"]
+    if any(word in f for word in delete):
+        df.drop(f, axis=1, inplace=True)
+    else:
+        if "Attire" in f:
+            df[f] = df[f].replace(['casual', 'dressy'], [0,1])
+        
+        if "Noise" in f:
+            df[f] = df[f].replace(['quiet', 'average', 'loud', 'very_loud'], [0,1,2,3])
+        
+        if "Wi-Fi" in f:
+            df[f] = df[f].replace(['no', 'free'], [0,1]) 
 
-# Fill the empty cells with the mode of category (one that appears the most)
-#waiter.fillna(mode(waiter).mode[0], inplace = True)
-waiter = waiter.astype(int)
+        if "Alcohol" in f:
+            df[f] = df[f].replace(['none', 'beer_and_wine', 'full_bar'], [0,1,2])
 
-casual_ambience = df["attributes.Ambience.casual"]
-casual_ambience.fillna(0, inplace = True)
-#casual_ambience.fillna(mode(casual_ambience).mode[0], inplace = True)
-casual_ambience = casual_ambience.astype(int)
+        df[f].fillna(0, inplace = True)
+        if "FALSE" in df[f].tolist(): #Convert true/false to integer values
+            df[f] = df[f].astype(int)
+        
+
+#alc = df["attributes.Alcohol"].replace(['none', 'beer_and_wine', 'full_bar'], [0,1,2])
+#alc.fillna(mode(alc).mode[0], inplace = True)
+#
+#takeout = df["attributes.Take-out"]
+#takeout.fillna(0, inplace = True)
+#takeout = takeout.astype(int)
+#
+#waiter = df["attributes.Waiter Service"]
+## If restaurant does not list a benefit, we assume that it lacks it
+#waiter.fillna(0, inplace = True)
+#
+## Fill the empty cells with the mode of category (one that appears the most)
+##waiter.fillna(mode(waiter).mode[0], inplace = True)
+#waiter = waiter.astype(int)
+#
+#casual_ambience = df["attributes.Ambience.casual"]
+#casual_ambience.fillna(0, inplace = True)
+##casual_ambience.fillna(mode(casual_ambience).mode[0], inplace = True)
+#casual_ambience = casual_ambience.astype(int)
 
 frames = numpy.column_stack((takeout, waiter, casual_ambience, alc))
 X = frames[1:500]
 print(X)
 ytrain = y[1:500]
 
-print(frames[2000])
-
 #for kernel in ('linear', 'poly', 'rbf'):
 
 clf = svm.SVC()
 clf.fit(X,ytrain)
 
-print(frames[1000])
 
 numTestData = 1000
 ytest = y[1000:2000]
-print(ytest.shape)
 
 predictions = []
 predictions = clf.predict(frames[1000:2000])
-print(predictions.shape)
 
-    
 error = sum(ytest * predictions <= 0) / numTestData
 print("Test error for SVM: " + str(error))
 
@@ -85,17 +120,3 @@ print("Test error for SVM: " + str(error))
 #plt.scatter(X[:, 0], X[:, 1], c = y)
 #plt.legend()
 #plt.show()
-
-
-
-    
-    
-
-
-
-
-
-
-
-
-
