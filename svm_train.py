e0d59d83ab76378a0f8f340b103902de4d6fcdf2
angelakong <akong2@stanford.edu>
2016-05-20 17:28:16 -0700

    Finished preprocessing

--- svm_train.py
+++ svm_train.py
@@ -19,51 +19,57 @@ import matplotlib.pyplot as plt
 
 df = pandas.read_csv('yelp_academic_dataset_business.csv', skip_blank_lines=True)
 # There are 77445 training examples, 98 possible features
+sparse = df.copy(deep=True)
 
 features = df.columns.values
 
 #for index, feature in enumerate(features):
-#    if "categories" in feature:
+#    if "city" in feature:
 #        print index
-        
+#        
 category_index = 20
+city_index = 58
 
 # Classification vector
 y = df['stars']
 y[y < 4.0] = 0
 y[y >= 4.0] = 1
 
-# Remove the non-restaurants from the data
-for i, category in enumerate(df["categories"]):
-    if "u'Restaurants" not in category:
-        df.drop(df.index[i])
 
-print(df.shape)
+# Only consider the training data if in a certain city, and is of category: restaurant
+#for i, row in sparse.iterrows():
+#    if "u'Restaurants" not in row[category_index]:
+#        sparse.drop(sparse.index[i])
+#    if "Pittsburgh" not in row[city_index]:
+#        sparse.drop(sparse.index[i])
 
 # Preprocess the data for modeling
-# Prepare for messy code
 for f in features:
     # Special code for categories
-    # Drop hours for now (easier to process)
-    delete = ["hours", "business_id", "stars", "type", "categories", "name", "full_address", "BYOB", "neighborhoods"]
+    delete = ["hours", "city", "state", "business_id", "stars", "type", "categories", "name", "full_address", "BYOB", "neighborhoods"]
+    
     if any(word in f for word in delete):
-        df.drop(f, axis=1, inplace=True)
+        sparse.drop(f, axis=1, inplace=True)
     else:
+        
+        if "Smoking" in f:
+            sparse[f] = sparse[f].replace(['no', 'outdoor', 'yes'], [0,1,2])
+            
         if "Attire" in f:
-            df[f] = df[f].replace(['casual', 'dressy'], [0,1])
+            sparse[f] = sparse[f].replace(['casual', 'dressy', 'formal'], [0,1,2])
         
         if "Noise" in f:
-            df[f] = df[f].replace(['quiet', 'average', 'loud', 'very_loud'], [0,1,2,3])
+            sparse[f] = sparse[f].replace(['quiet', 'average', 'loud', 'very_loud'], [0,1,2,3])
         
         if "Wi-Fi" in f:
-            df[f] = df[f].replace(['no', 'free'], [0,1]) 
+            sparse[f] = sparse[f].replace(['no', 'paid', 'free'], [0,1,2]) 
 
         if "Alcohol" in f:
-            df[f] = df[f].replace(['none', 'beer_and_wine', 'full_bar'], [0,1,2])
+            sparse[f] = sparse[f].replace(['none', 'beer_and_wine', 'full_bar'], [0,1,2])
 
-        df[f].fillna(0, inplace = True)
-        if "FALSE" in df[f].tolist(): #Convert true/false to integer values
-            df[f] = df[f].astype(int)
+        sparse[f].fillna(0, inplace = True)
+        if "FALSE" in sparse[f].tolist(): #Convert true/false to integer values
+            sparse[f] = sparse[f].astype(int)
         
 
 #alc = df["attributes.Alcohol"].replace(['none', 'beer_and_wine', 'full_bar'], [0,1,2])
@@ -86,9 +92,7 @@ for f in features:
 ##casual_ambience.fillna(mode(casual_ambience).mode[0], inplace = True)
 #casual_ambience = casual_ambience.astype(int)
 
-frames = numpy.column_stack((takeout, waiter, casual_ambience, alc))
-X = frames[1:500]
-print(X)
+X = sparse[1:500]
 ytrain = y[1:500]
 
 #for kernel in ('linear', 'poly', 'rbf'):
@@ -96,27 +100,11 @@ ytrain = y[1:500]
 clf = svm.SVC()
 clf.fit(X,ytrain)
 
-
 numTestData = 1000
 ytest = y[1000:2000]
 
 predictions = []
-predictions = clf.predict(frames[1000:2000])
+predictions = clf.predict(sparse[1000:2000])
 
 error = sum(ytest * predictions <= 0) / numTestData
 print("Test error for SVM: " + str(error))
-
-#clf = svm.SVC(kernel = 'linear', gamma = 2)
-#clf.fit(X,y)
-#plt.figure(fignum, figsize = (4,3))
-#    
-#w = clf.coef_[0]
-#    
-#a = -w[0] / w[1]
-#    
-#xx = numpy.linspace(0,12)
-#yy = a * xx - clf.intercept_[0] / w[1]
-#h0 = plt.plot(xx, yy, 'k-', label="non weighted div")
-#plt.scatter(X[:, 0], X[:, 1], c = y)
-#plt.legend()
-#plt.show()
